250ab96fba88b4f7d5139a00cf3dea14a6e8bb5b
LIHUICHAOlihuichao <2686079948@qq.com>
2020-10-04 20:41:14 +0800

    work

---
+++ build/precache-manifest.4148642f6ebb2f38997b33dfe17195d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdcf49d5c130a466b18e903a4276d714",
+    "url": "/index.html"
+  },
+  {
+    "revision": "841076c9be0ff71aa1cc",
+    "url": "/static/js/2.7b9aef16.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7b9aef16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29eebb5b52704f689f47",
+    "url": "/static/js/main.e510a1c1.chunk.js"
+  },
+  {
+    "revision": "711714d7d018d95c9ba4",
+    "url": "/static/js/runtime-main.33471999.js"
+  }
+]);
